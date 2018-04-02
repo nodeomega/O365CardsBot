4727ee5f125c13074aa351ed310c9a8b7902a55f
Jon Russell (Teksystems) <v-jonru@microsoft.com>
2018-04-02 16:29:36 -0700

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=v-jonru-nodebot-o365-Web-Deploy.pubxml -p:Password=MJpEzJeCtGrGHWiDXfdrpECrPbanTkuA5cWFT6KlyY6gh7PKNcEZeX9g1vmu
+
